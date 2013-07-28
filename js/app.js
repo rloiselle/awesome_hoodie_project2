78611af41f54fb152d6d8a1a9bd293fa0249ca76
hakimu jackson <hakimuj@gmail.com>
2013-07-27 19:24:20 -0700

    Update buttons. Enabled submit button to change to green

--- js/app.js
+++ js/app.js
@@ -5,38 +5,47 @@ $(document).ready(function(){
   });
 
   $('.dropdown-toggle').dropdown();
- 
+
+ var counter = 0;
   $('#school-logo .dropdown-menu li a').on('click',function(){
     $('#school-logo .btn:first-child').text($(this).text());
     $('#school-logo .btn:first-child').val($(this).text());
     // $('#school-logo .btn:first-child').html('<span class="caret"></span>');
-
+    counter = counter + 1;
 
   });
 
    $('#size .dropdown-menu li a').on('click',function(){
     $('#size .btn:first-child').text($(this).text());
     $('#size .btn:first-child').val($(this).text());
+    counter = counter + 1;
   });
 
   $('#quanity .dropdown-menu li a').on('click',function(){
     $('#quanity .btn:first-child').text($(this).text());
     $('#quanity .btn:first-child').val($(this).text());
+    counter = counter + 1;
+    if (counter == 3) {
+       $('#submit').addClass('btn btn-success');
+    }
   });
 
   $('#submit').on('click',function(){
     var schoolLogo = $('#school-logo .btn:first-child').val();
     var size = $('#size .btn:first-child').val();
     var quanity = $('#quanity .btn:first-child').val();
-    $('#submit').data("logo",schoolLogo);
-    $('#submit').data("size",size);
-    $('#submit').data("quanity",quanity);
-
+    if (counter == 3) {
+      $('#submit').data("logo",schoolLogo);
+      $('#submit').data("size",size);
+      $('#submit').data("quanity",quanity);
+    }
     console.log(schoolLogo);
     console.log(size);
     console.log(quanity);
     console.log($('#submit'));
     console.log(document.getElementById('submit'));
     console.log($('#submit').data());
+    console.log(counter);
+   
   });
 });
